447db8ae8a35f91341f21aaf6944dfb31d24be43
huang.cz <hnxyhcz@hotmail.com>
2022-05-26 09:54:33 +0800

    补提代码

--- change-demo-server/src/main/java/io/example/utils/JsonUtil.java
+++ change-demo-server/src/main/java/io/example/utils/JsonUtil.java
@@ -1,8 +1,5 @@
 package io.example.utils;
 
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -11,12 +8,13 @@ import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.nimbusds.jose.shaded.json.JSONObject;
-
 import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 
 /**
  * JSON转换工具
- * 
  * @author huang.cz
  * @since 2022/4/15 23:14
  */
@@ -36,14 +34,14 @@ public class JsonUtil {
 
     /**
      * 将对象序列化为JSON
-     *
      * @param object 对象
      * @return JSON格式
      */
     public static String toJSONString(Object object) {
         try {
             return objectMapper.writeValueAsString(object);
-        } catch (JsonProcessingException e) {
+        }
+        catch (JsonProcessingException e) {
             log.error("序列化失败!" + e.getMessage());
             return null;
         }
@@ -51,15 +49,15 @@ public class JsonUtil {
 
     /**
      * JSON反序列化对象
-     *
-     * @param str 反序列化的JSON串
+     * @param str   反序列化的JSON串
      * @param clazz 反序列化的目标对象
      * @return T 对象
      */
     public static <T> T parseObject(String str, Class<T> clazz) {
         try {
             return objectMapper.readValue(str, clazz);
-        } catch (IOException e) {
+        }
+        catch (IOException e) {
             log.error("反序列化失败", e);
             return null;
         }
@@ -67,9 +65,8 @@ public class JsonUtil {
 
     /**
      * JSON反序列化对象
-     *
      * @param jsonObj 反序列化的JSON对象
-     * @param clazz 反序列化的目标对象
+     * @param clazz   反序列化的目标对象
      * @return T 对象
      */
     public static <T> T parseObject(JSONObject jsonObj, Class<T> clazz) {
@@ -81,17 +78,17 @@ public class JsonUtil {
 
     /**
      * 带泛型的反序列化
-     *
-     * @param str 待序列化的字符串
+     * @param str        待序列化的字符串
      * @param superClazz superClazz
-     * @param clazz 泛型对象
+     * @param clazz      泛型对象
      * @return 序列化为泛型对象
      */
     public static <S, T> S parseObject(String str, Class<S> superClazz, Class<T> clazz) {
         JavaType javaType = getCollectionType(superClazz, clazz);
         try {
             return objectMapper.readValue(str, javaType);
-        } catch (JsonProcessingException e) {
+        }
+        catch (JsonProcessingException e) {
             log.error("反序列化失败", e);
             return null;
         }
@@ -100,5 +97,4 @@ public class JsonUtil {
     public static JavaType getCollectionType(Class<?> collectionClass, Class<?>... elementClasses) {
         return objectMapper.getTypeFactory().constructParametricType(collectionClass, elementClasses);
     }
-
 }
